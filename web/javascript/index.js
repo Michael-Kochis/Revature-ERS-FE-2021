2f51d6fa0a28abe17924d4645b25ec7888a98e76
Michael-Kochis <rhishisikk@gmail.com>
2021-09-19 09:36:23 -0500

    Added login and register buttons, no functionality yet

--- web/javascript/index.js
+++ web/javascript/index.js
@@ -1,11 +1,19 @@
-import {sayHi} from './sayHi'
+// import {sayHi} from './sayHi'
 
-// function sayHi(user) {
-//     return ("Hello " + user);
-// }
+function sayHi(user) {
+    return ("Hello " + user);
+}
 
 window.onload = function() {
-
     let root = document.getElementById('root');
     root.innerHTML = sayHi('World!');
+
+    let loginButton = document.createElement("button");
+    loginButton.innerText = "Login";
+
+    let registerButton = document.createElement("button");
+    registerButton.innerText = "Register";
+
+    root.appendChild(loginButton);
+    root.appendChild(registerButton);
 };
