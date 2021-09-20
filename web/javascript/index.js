c62029546c28bf7a0f782bef823be971ed812a67
Michael-Kochis <rhishisikk@gmail.com>
2021-09-20 10:00:00 -0500

    Added navbar and began faux form

--- web/javascript/index.js
+++ web/javascript/index.js
@@ -1,19 +1,54 @@
 // import {sayHi} from './sayHi'
 
-function sayHi(user) {
-    return ("Hello " + user);
+function fakeForm() {
+    let root = document.getElementById('root');
+    let fakeForm = document.createElement('div');
+    fakeForm.id = "fakeForm";
+
+    setLoginAndRegisterButtons(fakeForm);
+
+    root.appendChild(fakeForm);
+}
+
+function navBar() {
+    let root = document.getElementById('root');
+
+    let navBar = document.createElement('div');
+    navBar.id = "navBar";
+    navBar.classList.add("w10");
+
+    let homeLink = document.createElement('a');
+    homeLink.id = "homeLink";
+    homeLink.innerText = "Home";
+
+    navBar.appendChild(homeLink);
+    root.appendChild(navBar);
+}
+
+function setLoginAndRegisterButtons(parentNode) {
+    let root = document.getElementById('root');
+
+    let lrButtonBar = document.createElement('div');
+    lrButtonBar.id = "lrButtonBar"
+
+    let loginButton = document.createElement("button");
+    loginButton.id = "loginButton";
+    loginButton.innerText = "Login";
+
+    let registerButton = document.createElement("button");
+    registerButton.id = "registerButton";
+    registerButton.innerText = "Register";
+
+    lrButtonBar.appendChild(loginButton);
+    lrButtonBar.appendChild(registerButton);
+    parentNode.appendChild(lrButtonBar)
 }
 
 window.onload = function() {
     let root = document.getElementById('root');
-    root.innerHTML = sayHi('World!');
+    root.innerHTML = "";
 
-    let loginButton = document.createElement("button");
-    loginButton.innerText = "Login";
-
-    let registerButton = document.createElement("button");
-    registerButton.innerText = "Register";
-
-    root.appendChild(loginButton);
-    root.appendChild(registerButton);
+    navBar();
+    fakeForm();
+    navBar()
 };
